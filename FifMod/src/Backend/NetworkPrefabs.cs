98dc38931c6e391a1f03c39b44372c7b9f266956
zstardustttt <93951756+zSt4rdust@users.noreply.github.com>
2023-12-16 16:07:53 +0700

    removed useless usings

--- FifMod/src/Backend/NetworkPrefabs.cs
+++ FifMod/src/Backend/NetworkPrefabs.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using HarmonyLib;
 
 namespace FifMod
 {
--- FifMod/src/Backend/StoreItems.cs
+++ FifMod/src/Backend/StoreItems.cs
@@ -1,8 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
-using FifMod.Utils;
-using HarmonyLib;
-using UnityEngine;
 
 namespace FifMod
 {
